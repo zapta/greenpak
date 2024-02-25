bf4de7f770c43f44f11debb20d2f091b7487d1a9
Zapta <zapta@users.noreply.github.com>
2024-02-25 11:07:28 -0800

    Tweaked the read test.

--- test/read_test.py
+++ test/read_test.py
@@ -1,62 +1,78 @@
-
 import random
 from greenpak import driver, utils, i2c
 
-port = "/dev/tty.usbmodem1101"
-#port = "/dev/tty.usbserial-DK0C3UQC"
+VERBOSE = False
+SEED = 0
+ITERS = 10000
 
+
+def randomize_eeprom(gpd: driver.GreenpakDriver):
+    print(f"\n\n ------ Randomizing the EEPROM.")
+    if SEED is not None:
+        random.seed(SEED)
+    data = bytearray()
+    for _ in range(256):
+        data.append(random.randint(0, 255))
+    print("\nRandom data:")
+    utils.hex_dump(data)
+    print("\nProgramming the eeprom")
+    gpd.program_eeprom_pages(0, data)
+    print("Resetting the device.")
+    gpd.reset_device()
+
+
+def format_set(s:set):
+    l = list(s)
+    l.sort()
+    l_str = []
+    for b in l:
+        l_str.append(f"{b:02x}")
+    return str(l_str)
+
+def test_mem_space(name: str, read_method) -> None:
+    print(f"\n\n ------ Testing {name} memory space.")
+    if SEED is not None:
+        random.seed(SEED)
+    mem = read_method(0, 256)
+    good_set = set()
+    bad_set = set()
+    ok_count = 0
+    err_count = 0
+    print(f"Starting {ITERS} reads...")
+    for _ in range(ITERS):
+        a = random.randint(0, 255)
+        b = read_method(a, 1)[0]
+        ok = b == mem[a]
+        if ok:
+            status = "ok   "
+            ok_count += 1
+            good_set.add(a)
+        else:
+            status = "ERROR"
+            err_count += 1
+            bad_set.add(a)
+        # Verbose log
+        if VERBOSE or not ok:
+          print(
+              f"addr {a:02x}, ref: {mem[a]:02x}, read: {b:02x}  {status}     {ok_count}/{err_count}"
+          )
+    print(f"Ok  reads: {ok_count}")
+    print(f"Err reads: {err_count}")
+    print(f"Good address count: {len(good_set)}")
+    print(f"Good/bad addresses: {format_set(bad_set.intersection(good_set))}")
+    print(f"Bad only addresses: {format_set(bad_set)}")
+
+
+# Main
+    
 print("\nConnecting.", flush=True)
-i2c_driver = i2c.GreenPakI2cAdapter(port=port)
-#i2c_driver = i2c.GreenPakI2cDriver(port=port)
-
+i2c_driver = i2c.GreenPakI2cAdapter(port="/dev/tty.usbmodem1101")
 gp_driver = driver.GreenpakDriver(
-    i2c_driver, device_type="SLG46826", device_control_code=1)
+    i2c_driver, device_type="SLG46826", device_control_code=1
+)
 
-
-print("\nReading entire mem space.", flush=True)
-#mem = gp_driver.read_eeprom_bytes(0, 256)
-#mem = gp_driver.read_nvm_bytes(0, 256)
-mem = gp_driver.read_register_bytes(0, 256)
-
-
-bad_set = set()
-
-random.seed(0)  # Repeatable rand.
-ok_count = 0
-err_count = 0
-#for i in range(256):
-for _ in range(5000):
-    #a = 255 - i
-    a = random.randint(0, 255)
-    #b = gp_driver.read_eeprom_bytes(a, 1)[0]
-    #b = gp_driver.read_nvm_bytes(a, 1)[0]
-    b = gp_driver.read_register_bytes(a, 1)[0]
-    if b == mem[a]:
-      ok = True
-      status = "ok   "
-      ok_count += 1
-    else:
-       ok = False
-       status = "ERROR"
-       err_count += 1
-       bad_set.add(a)
-
-       
-    print(f"addr {a:02x}, ref: {mem[a]:02x}, read: {b:02x}  {status}     {ok_count}/{err_count}")
-    if not ok:
-      for i in range(5):
-         b = gp_driver.read_eeprom_bytes(a, 1)[0]
-         print(f"  {b:02x}")
-
-print(f"Ok: {ok_count}, Errors: {err_count}")
-bad_list = list(bad_set)
-bad_list.sort()
-for a in bad_list:
-   print(f"Bad address: {a:02x}")
-bad_list_str = []
-for a in bad_list:
-   bad_list_str.append(f"{a:02x}")
-print(f"Bad addresses: {bad_list_str}")
-
-
+randomize_eeprom(gp_driver)
+test_mem_space("EEPROM", gp_driver.read_eeprom_bytes)
+test_mem_space("NVM", gp_driver.read_nvm_bytes)
+test_mem_space("REGISTER", gp_driver.read_register_bytes)
 
